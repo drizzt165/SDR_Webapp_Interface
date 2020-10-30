989b9a525b6ebdc61a3ec70fffde413cd60e2490
Nick Brannies <nick.brannies@gmail.com>
2020-10-29 20:40:13 -0700

    Initial commit with web form approach

--- webapp.py
+++ webapp.py
@@ -6,22 +6,27 @@ from flask import Flask, request, render_template, redirect, url_for
 #sock.bind("tcp://*:9101")
 app = Flask(__name__)
 
-pastMsgs = ['Hello! Are you out there?']
+pastMsgs = ['Hello! Are you out there?'] #default msg for chat log
 
 @app.route('/')
 def home():
-    history = []
     return render_template('my-form.html',chatLog = ('<br/>'.join(pastMsgs)))
 
 @app.route('/', methods=['POST'])
 def my_form_post(prev_msg = None):
-    text = request.form['text']
-    processed_text = text #Change text if we desire
-    #sock.send_string(processed_text)
-    pastMsgs.append(processed_text)
-    return redirect(url_for("home",prev_msg = processed_text))
+    name = request.form['name']
+    message = request.form['message']
+    location = request.form['location']
+
+    allInfo = [name,message,location]
+    stringInfo = ' | '.join(allInfo)
+    print(allInfo)
+
+    #sock.send_string(stringInfo)
+    pastMsgs.append(message)
+    return redirect(url_for("home",prev_msg = message))
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0')
+    app.run()
     #sock.close()
     #ctx.term()
