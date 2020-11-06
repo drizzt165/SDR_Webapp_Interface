ee2406867e3d261506232bbff439b8265c76cd8d
Nick Brannies <nick.brannies@gmail.com>
2020-11-06 14:50:59 -0800

    Added CSV support

--- webapp.py
+++ webapp.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
+import csv
+import os.path
+from os import path
 #import zmq
 
 #ctx = zmq.Context()
@@ -20,11 +23,22 @@ def my_form_post(prev_msg = None):
 
     allInfo = [name,message,location]
     stringInfo = ' | '.join(allInfo)
-    print(allInfo)
+    csvDump(allInfo)
+    print(stringInfo)
 
     #sock.send_string(stringInfo)
     pastMsgs.append(message)
     return redirect(url_for("home",prev_msg = message))
+
+def csvDump(info):
+    if(not (os.path.isfile('info.csv'))):
+        with open(r'info.csv', 'a',newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['name','message','location'])
+
+    with open(r'info.csv', 'a',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(info)
 
 if __name__ == "__main__":
     app.run()
